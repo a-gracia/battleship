c43361a53d562e7569b60b68853469eab050f85e
Andrés Gracia Danies <18741308+a-gracia@users.noreply.github.com>
2026-01-28 17:14:25 -0500

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import globals from "globals";
+import css from "@eslint/css";
+import prettier from "eslint-config-prettier";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    ...js.configs.recommended,
+  },
+
+  {
+    files: ["**/*.css"],
+    language: "css/css",
+    ...css.configs.recommended,
+  },
+
+  prettier,
+];
